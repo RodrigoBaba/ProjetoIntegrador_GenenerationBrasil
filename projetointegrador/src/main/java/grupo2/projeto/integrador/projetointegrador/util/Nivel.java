13b958427bd14609d1c22858e29c5488224536c7
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2022-01-21 10:33:25 -0300

    Começando projeto integrador

---
+++ projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/util/Nivel.java
@@ -0,0 +1,5 @@
+package grupo2.projeto.integrador.projetointegrador.util;
+
+public enum Nivel {
+	AVANCADO, INTERMEDIARIO, BASICO, SENIOR, PLENO, JUNIOR;
+}
---
+++ projetointegrador/src/test/java/grupo2/projeto/integrador/projetointegrador/ProjetointegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.projeto.integrador.projetointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetointegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
