1a802afd8f33b1293e58b54726759f4f42671e01
isabelalberti <isabelalberti84@gmail.com>
2022-01-26 12:25:04 -0300

    @isabelalberti [Criando método findById]

--- projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/controller/SkillController.java
+++ projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/controller/SkillController.java
@@ -1,12 +1,14 @@
 package grupo2.projeto.integrador.projetointegrador.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -34,5 +36,11 @@ public class SkillController {
 		}
 
 	}
+
+	@GetMapping("/select/{id}")
+
+	public ResponseEntity<Skill> findById(@PathVariable("id") Long idvariavel) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.findById(idvariavel).get());
+		 }
 
 }
