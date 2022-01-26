9f4b2b1847562e86677110b493d7ebf2e92aa406
Lucas <watchlucas274@gmail.com>
2022-01-26 14:48:23 -0300

    @Lucxsss[Criando metodo put]

--- projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/controller/SkillController.java
+++ projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/controller/SkillController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -62,5 +64,14 @@ public class SkillController {
 	public ResponseEntity<Skill> findById(@PathVariable("id") Long idvariavel) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.findById(idvariavel).get());
 		 }
+
+	@PutMapping("/alterar/{id}")
+	public ResponseEntity<Skill> putSkill(@RequestBody Skill altSkill) {
+		return repository.findById(altSkill.getId()).map(resp -> ResponseEntity.status(200)
+				.body(repository.save(altSkill)))
+				.orElseGet(() -> {
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID não encontrado");
+				});
+	}
 
 }
