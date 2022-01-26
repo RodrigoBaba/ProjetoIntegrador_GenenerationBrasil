2a3e0be1e9f92ea98154e07c7cfb017446ba911c
Gabriel Luis Nolasco <gl450681@gmail.com>
2022-01-26 12:34:52 -0300

    @BielIl [Add delete at Controller]
    adicionando o delete no controller

--- projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/controller/SkillController.java
+++ projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/controller/SkillController.java
@@ -1,12 +1,15 @@
 package grupo2.projeto.integrador.projetointegrador.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -34,5 +37,24 @@ public class SkillController {
 		}
 
 	}
+	
+	//DELETE
+		@DeleteMapping("/delete/{id}")
+		public ResponseEntity<?> delete(@PathVariable Long id){
+			
+			Optional<Skill> optional = repository.findById(id);
+			
+				if (optional.isPresent()) {
+					
+					repository.deleteById(id);
+					return ResponseEntity.status(200).build();
+					
+				} else {
+					
+					throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Id não encontrado!");
+					
+				}
+				
+		}
 
 }
