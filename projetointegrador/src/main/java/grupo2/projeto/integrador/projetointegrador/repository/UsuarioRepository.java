480540f20c458e70e511100e8fcf4f3d592de7b9
Lucas <watchlucas274@gmail.com>
2022-01-27 11:01:57 -0300

    @Lucxsss[Criando repository]

---
+++ projetointegrador/src/main/java/grupo2/projeto/integrador/projetointegrador/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package grupo2.projeto.integrador.projetointegrador.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import grupo2.projeto.integrador.projetointegrador.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	public List<Usuario> findByCityContainingIgnoreCase(String cityUser);
+	
+	public List<Usuario> findAllCityContainingIgnoreCase(String cityUser);
+}
