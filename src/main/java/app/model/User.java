16dd41c9cfa401de8e2d7791d7f9b21550156b79
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2022-03-10 10:48:33 -0300

    @RodrigoBaba [Arrumando erro no Put de User]

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -1,14 +1,21 @@
 package app.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
